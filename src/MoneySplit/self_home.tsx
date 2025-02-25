053aa90aa60c3f2e168834f8f568a0fe1fd99132
inthhh <glass005533@gmail.com>
2025-02-25 17:45:44 +0900

    feat: 월급쪼개기 ai/self 설정 UI 생성

---
+++ src/MoneySplit/self_home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./splitStyle.css";
+import { useNavigate } from "react-router-dom";
+
+function Self_home() {
+  return <div>ddd</div>;
+}
+
+export default Self_home;
