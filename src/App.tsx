cca1cab512e7bf3520700c694574c2b4bfaa9af2
inthhh <glass005533@gmail.com>
2025-02-26 17:36:18 +0900

    충돌 해결 및 merge

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,6 @@ import Authentication from "./MoneySplit/Authentication";
 import TossAuth from "./MoneySplit/TossAuth";
 import InputPin from "./MoneySplit/InputPin";
 
-import AI_1 from "./MoneySplit/ai_1";
-import Self_1 from "./MoneySplit/self_1";
 import Account from "./Main/account";
 import Second from "./Main/second";
 
@@ -27,13 +25,12 @@ import SalaryInfoPage from "./SignOn/pages/SalaryInfoPage";
 import InterestPage from "./SignOn/pages/InterestPage";
 import CompletionPage from "./SignOn/pages/CompletionPage";
 
+import AddCategory from "./MoneySplit/AddCategory";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div
-        style={{ width: "400px", height: "800px", backgroundColor: "white" }}
-      >
+      <div style={{ width: "400px", height: "800px", backgroundColor: "white" }}>
         <Routes>
           <Route path="/" element={<First />} />
           <Route path="/MoneySplit" element={<MoneySplitFirst />} />
@@ -49,8 +46,7 @@ const App: React.FC = () => {
           <Route path="/MoneySplit/Authentication" element={<Authentication />} />
           <Route path="/MoneySplit/TossAuth" element={<TossAuth />} />
           <Route path="/MoneySplit/InputPin" element={<InputPin />} />
-          <Route path="/MoneySplit/AI/1" element={<AI_1 />} />
-          <Route path="/MoneySplit/Self/1" element={<Self_1 />} />
+
           <Route path="/Account" element={<Account />} />
           <Route path="/Second" element={<Second />} />
 
@@ -61,6 +57,7 @@ const App: React.FC = () => {
           <Route path="/sign/interest" element={<InterestPage />} />
           <Route path="/sign/completion" element={<CompletionPage />} />
 
+          <Route path="/MoneySplit/AddCategory" element={<AddCategory />} />
         </Routes>
       </div>
     </Router>
---
+++ src/MoneySplit/AddCategory.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import { motion } from "framer-motion";
+import "./splitStyle.css"; // CSS 파일 import
+import { useNavigate } from "react-router-dom";
+import LoadingSpinner from "./loadingSpinner";
+
+const AddCategory: React.FC = () => {
+  return <div>카테고리 추가</div>;
+};
+
+export default AddCategory;
