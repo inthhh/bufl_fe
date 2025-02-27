a12d94be1d1e0d60711ca05f7bdb89356213ef74
hyungeerhee <hyungeerhee@naver.com>
2025-02-27 13:28:22 +0900

    feat: 관심사 선택 UI 개선 (단일 선택 및 이미지 클릭 가능)

--- src/SignOn/pages/CompletionPage.tsx
+++ src/SignOn/pages/CompletionPage.tsx
@@ -1,11 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import "../style/style.css";
-
+import SuccessImg from "../images/success.png"
 function CompletionPage() {
   const navigate = useNavigate();
 
   return (
     <div>
+      <img src={SuccessImg} alt="success" width="140px"/>
       <h2>가입 완료!</h2>
       <button onClick={() => navigate("/")}>메인 화면으로</button>
     </div>
