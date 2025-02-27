7be2a72924b64f3ed7710a92280c3f8c78ce7298
inthhh <glass005533@gmail.com>
2025-02-27 13:32:01 +0900

    feat: PIN 입력 페이지 생성

--- src/MoneySplit/InputPin.tsx
+++ src/MoneySplit/InputPin.tsx
@@ -1,18 +1,63 @@
-import React from "react";
-import "./splitStyle.css";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./splitStyle.css";
 
 const InputPin: React.FC = () => {
+  const [pin, setPin] = useState<string[]>(Array(6).fill(""));
   const navigate = useNavigate();
-  const clickToNext = () => {
-    navigate("/");
+
+  const handleKeyPress = (num: string) => {
+    setPin((prev) => {
+      const newPin = [...prev];
+      const emptyIndex = newPin.findIndex((val) => val === "");
+      if (emptyIndex !== -1) newPin[emptyIndex] = num;
+      return newPin;
+    });
   };
+
+  const handleDelete = () => {
+    setPin((prev) => {
+      const newPin = [...prev];
+      const lastIndex = newPin.lastIndexOf("");
+      const targetIndex = lastIndex === -1 ? 5 : lastIndex - 1;
+      if (targetIndex >= 0) newPin[targetIndex] = "";
+      return newPin;
+    });
+  };
+
+  const navigateToNext = () => {
+    // 모든 요소가 true일때 true
+    if (pin.every((p) => p !== "")) navigate("/MoneySplit/LastLoading");
+  };
+
   return (
     <div className="center_wrap">
       <div>
-        자동이체 등록을 위해
-        <br />
-        PIN 번호를 입력해주세요.
+        <div className="pin-message">
+          자동이체 등록을 위해 <br />
+          PIN 번호를 입력해주세요.
+        </div>
+        <div className="pin-input-container">
+          {pin.map((num, index) => (
+            <input key={index} type="text" value={num} readOnly className="pin-input" />
+          ))}
+        </div>
+        <div className="keypad">
+          {[...Array(9)].map((_, index) => (
+            <button key={index + 1} className="keypad-button" onClick={() => handleKeyPress((index + 1).toString())}>
+              {index + 1}
+            </button>
+          ))}
+          <button className="keypad-button" onClick={handleDelete}>
+            ⬅
+          </button>
+          <button className="keypad-button" onClick={() => handleKeyPress("0")}>
+            0
+          </button>
+          <button className="keypad-button confirm" onClick={navigateToNext}>
+            ✔
+          </button>
+        </div>
       </div>
     </div>
   );
