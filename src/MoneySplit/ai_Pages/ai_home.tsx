7b1bdbf10971ce3497b1d4f85b0bedd53223a23c
inthhh <glass005533@gmail.com>
2025-02-28 13:02:21 +0900

    style: ai home 스타일 수정

--- src/MoneySplit/ai_Pages/ai_home.tsx
+++ src/MoneySplit/ai_Pages/ai_home.tsx
@@ -14,12 +14,18 @@ const AI_home: React.FC = () => {
       <MoveBack pageBefore="/money-split" />
       <div className="center_wrap">
         <div>
-          <div className="black_title">AI가 추천해주는 월급 쪼개기</div>
-          <div>
+          <div className="black_title_big">
+            <p className="center_text" style={{ margin: "50px 0 40px 0" }}>
+              AI가 추천해주는
+              <br />
+              월급 쪼개기
+            </p>
+          </div>
+          <div className="center_wrap">
             <img src={RobotImg} alt="" width={"150px"} />
           </div>
-          <div>
-            <p>
+          <div className="center_wrap" style={{ marginTop: "220px" }}>
+            <p className="center_text">
               지난 소비 습관을 분석하고,
               <br />
               적절한 월급 쪼개기 비율을 자동으로 추천해 드릴게요.
@@ -27,9 +33,13 @@ const AI_home: React.FC = () => {
               나중에 비율을 수정할 수 있어요.
             </p>
           </div>
-          <button className="blue_btn" type="button" onClick={() => clickToNext()}>
-            시작하기
-          </button>
+          <div className="center_wrap">
+            <div className="center_wrap btn">
+              <button className="blue_big_btn" type="button" onClick={() => clickToNext()}>
+                시작하기
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
