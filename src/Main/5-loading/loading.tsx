13fec7453c848accd51662d63df481e4a3852889
songsong822 <shsuevfs@naver.com>
2025-03-06 13:13:15 +0900

    feat: 선택창들

--- src/Main/5-loading/loading.tsx
+++ src/Main/5-loading/loading.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./loadingStyle.css";
+import { useNavigate } from "react-router-dom";
 
 const Loading: React.FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/Main/list");
+    }, 2000); //2초 뒤에 이동
+
+    return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
+  }, [navigate]);
   return (
     <div className="load">
       <div className="spin">AI 추천받기</div>
